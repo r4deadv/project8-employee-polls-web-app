dcf7238c8baad95ab369f91eae584a1cce1ccbdc
r4deadv <mihovilovic.rade@gmail.com>
2022-12-08 18:13:22 +0100

    project middleware added

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from "./components/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./redux/reducers";
+import middleware from "./redux/middleware";
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
